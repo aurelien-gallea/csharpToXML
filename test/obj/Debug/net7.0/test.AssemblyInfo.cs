f70c51adba94e61f943e3fbd61c89c9308385a58
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-06-13 00:16:55 +0200

    ajout d'un programme console csharp à un fichier xml

--- test/obj/Debug/net7.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net7.0/test.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Exercice2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Exercice2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Exercice2")]
+[assembly: System.Reflection.AssemblyProductAttribute("test")]
+[assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
